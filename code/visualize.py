adc59ae2a121a06c86b4dd1ee75f79ba44b5f8aa
Lasse Regin Nielsen <lasseregin@gmail.com>
2016-10-17 21:51:00 +0200

    [Refactor] Removed HDF5 validation class and fitted visualize script to new frame loader.

--- code/visualize.py
+++ code/visualize.py
@@ -6,18 +6,14 @@ import data
 import utils
 from matplotlib import pyplot as plt
 
-frame_loader = data.FrameLoader(filename='GOPR2477', downsample=2)
-frame_loader = utils.BallFoundFilter(frame_iterator=frame_loader)
-frame_loader = utils.BallPositionPDF(frame_iterator=frame_loader)
+frame_loader = data.FrameLoader(shuffle=True)
 
-for i, (image, pdf) in enumerate(frame_loader):
+for i, (image, target) in enumerate(frame_loader):
     fig = plt.figure(figsize=(20,10))
-    plt.subplot(2, 1, 1)
-    plt.imshow(pdf)
-    plt.subplot(2, 1, 2)
-    plt.imshow(image)
+    print('Pixel coordinates')
+    print(target[0]*image.shape[1],target[1]*image.shape[0])
+    plt.imshow(image, cmap='gray')
 
     plt.show()
-
     if i > 20:
         break
