67999ae3051cc6fb06fb90fb9f3d69765a6f54e4
Lasse Regin Nielsen <lasseregin@gmail.com>
2016-10-30 20:57:23 +0100

    [Feature] Added evaluation script for convolutional regressor.

---
+++ code/conv_regressor_eval.py
@@ -0,0 +1,37 @@
+
+import os
+import data
+import utils
+import network
+
+import numpy as np
+import tensorflow as tf
+
+# Intialize frame loader
+frame_loader = data.FrameLoader(shuffle=True, validation_group='test')
+height, width = frame_loader.data.target_height, frame_loader.data.target_width
+
+frame_loader = utils.MinibatchUncached(frame_iterator=frame_loader)
+
+# Setup network
+nn = network.ConvolutionalRegressor(name='conv-model-1',
+                                    input_shape=(None, height, width),
+                                    target_shape=(None, 2))
+
+
+with tf.Session() as sess:
+    nn.load(sess)
+
+    test_loss = 0.
+    test_batches = 0
+    for images, targets in frame_loader:
+        images /= images.std() # TODO: do somewhere else
+        test_loss += nn.val_op(session=sess, x=images, y=targets)
+        predictions = nn.predict(session=sess, x=images)
+        print('predictions')
+        print(predictions)
+        print('')
+        test_batches += 1
+    test_loss /= test_batches
+
+    print('Test loss: %g' % (test_loss))
