2c513dbdbc8a5419fa60e1996d8bff041e043824
Lasse Regin Nielsen <lasseregin@gmail.com>
2016-11-07 16:23:03 +0100

    [Feature] Added evalution/plotting script for training.

--- code/eval_training.py
+++ code/eval_training.py
@@ -1,17 +1,35 @@
 
 import os
-import data
-import math
+import sys
+
+import matplotlib
+if 'DISPLAY' not in os.environ:
+    matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 import utils
-import network
 
-import numpy as np
-import tensorflow as tf
+MODEL_NAME = 'simple-model-1'
+if len(sys.argv) == 1:
+    print('No model name provided as argument. Using default value of \"%s\"' % (MODEL_NAME))
+else:
+    MODEL_NAME = sys.argv[1]
 
+print('Creating plot for model %s' % (MODEL_NAME))
 
 # Load training losses
-lossTracker = utils.LossTracker.load(name='simple-model-1')
+lossTracker = utils.LossTracker.load(name=MODEL_NAME)
 
-print(lossTracker.train_loss)
-print(lossTracker.val_loss)
-print(lossTracker.test_loss)
+plt.plot(lossTracker.train_loss, color='IndianRed', label='Train')
+plt.plot(lossTracker.val_loss,   color='SteelBlue', label='Val')
+plt.legend()
+
+if lossTracker.test_loss is not None:
+    plt.title('Final test error %g' % (lossTracker.test_loss))
+else:
+    plt.title('Final test error not available')
+
+# Save plot
+plt.savefig(os.path.join(lossTracker.RUNS_FOLDER, '%s.png') % (MODEL_NAME), bbox_inches='tight')
+plt.savefig(os.path.join(lossTracker.RUNS_FOLDER, '%s.pdf') % (MODEL_NAME), bbox_inches='tight')
+plt.show()
