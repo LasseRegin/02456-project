9e68c4c25d768ec35187792ba27f654196a080ba
Lasse Regin Nielsen <lasseregin@gmail.com>
2016-11-14 11:53:09 +0100

    tmp.

--- code/network/logistic.py
+++ code/network/logistic.py
@@ -9,27 +9,25 @@ class LogisticClassifier(Network):
         super().__init__(**kwargs)
 
     def init_variables(self):
-        with tf.device('/gpu:1'):
-            total_pixels = np.prod(self.input_shape[1:])
-            self.x_reshaped = tf.reshape(self.x, shape=[-1, total_pixels])
+        total_pixels = np.prod(self.input_shape[1:])
+        self.x_reshaped = tf.reshape(self.x, shape=[-1, total_pixels])
 
-            # Weights for layer 1
-            # NOTE: Gradients seems more stable when initializing W to zeros.
-            #       This can only be used with logistic regression.
-            #self.W_1 = tf.Variable(tf.random_normal([total_pixels, self.target_shape[1]], stddev=0.35), name='weights-layer-1')
-            #self.W_1 = tf.Variable(tf.random_normal([total_pixels, self.target_shape[1]], stddev=0.10), name='weights-layer-1')
-            self.W_1 = tf.Variable(tf.zeros([total_pixels, self.target_shape[1]]), name='weights-layer-1')
-            self.b_1 = tf.Variable(tf.zeros([self.target_shape[1]]), name='biases-layer-1')
+        # Weights for layer 1
+        # NOTE: Gradients seems more stable when initializing W to zeros.
+        #       This can only be used with logistic regression.
+        #self.W_1 = tf.Variable(tf.random_normal([total_pixels, self.target_shape[1]], stddev=0.35), name='weights-layer-1')
+        #self.W_1 = tf.Variable(tf.random_normal([total_pixels, self.target_shape[1]], stddev=0.10), name='weights-layer-1')
+        self.W_1 = tf.Variable(tf.zeros([total_pixels, self.target_shape[1]]), name='weights-layer-1')
+        self.b_1 = tf.Variable(tf.zeros([self.target_shape[1]]), name='biases-layer-1')
 
     def init_network(self):
 
-        with tf.device('/gpu:1'):
-            # Layer 1
-            a_1 = tf.matmul(self.x_reshaped, self.W_1) + self.b_1
-            z_1 = tf.nn.softmax(a_1)
+        # Layer 1
+        a_1 = tf.matmul(self.x_reshaped, self.W_1) + self.b_1
+        z_1 = tf.nn.softmax(a_1)
 
-            # Final output
-            self.output = z_1
+        # Final output
+        self.output = z_1
 
     def init_optimizer(self):
         # Minimize cross entropy
