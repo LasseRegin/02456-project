73d04fc391d9d3501fa45bc9188e4e9415da0874
Lasse Regin Nielsen <lasseregin@gmail.com>
2016-11-15 20:48:36 +0100

    Fixed rnn script.

--- code/rnn_features_classifier.py
+++ code/rnn_features_classifier.py
@@ -31,68 +31,6 @@ cells_y = frame_loader.cells_y
 # Split in train, validation and test set
 frame_loader = utils.SequenceValidationMinibatches(frame_iterator=frame_loader, batch_size=BATCH_SIZE)
 
-
-
-print('train')
-for input_seqs, targets in frame_loader.train:
-    print(input_seqs.shape)
-    print(targets.shape)
-
-print('----------')
-print('')
-
-print('val')
-for input_seqs, targets in frame_loader.val:
-    print(input_seqs.shape)
-    print(targets.shape)
-print('----------')
-print('')
-
-print('test')
-for input_seqs, targets in frame_loader.test:
-    print(input_seqs.shape)
-    print(targets.shape)
-
-
-import sys; sys.exit()
-
-#
-#
-# class Minibatches:
-#     def __init__(self, frame_seq_loader, batch_size=20):
-#         self.input_batches = []
-#         self.target_batches = []
-#         self.batch_size = batch_size
-#         inputs_batch = []
-#         target_batch = []
-#         count = 0
-#         for i, (frame_seq, target) in enumerate(frame_seq_loader):
-#             inputs_batch.append(frame_seq)
-#             target_batch.append(target)
-#             count += 1
-#             if count == self.batch_size:
-#                 self.input_batches.append(np.asarray(inputs_batch))
-#                 self.target_batches.append(np.asarray(target_batch))
-#                 inputs_batch = []
-#                 target_batch = []
-#                 count = 0
-#         if count > 0:
-#             self.input_batches.append(np.asarray(inputs_batch))
-#             self.target_batches.append(np.asarray(target_batch))
-#             inputs_batch = []
-#             target_batch = []
-#
-#     def __iter__(self):
-#         for inputs, targets in zip(self.input_batches, self.target_batches):
-#             yield inputs, targets
-#
-# frame_loader = Minibatches(frame_seq_loader=frame_loader, batch_size=BATCH_SIZE)
-
-# TODO: Make validation work
-## Split in train, validation and test
-#frame_loader = utils.ValidationMinibatches(frame_iterator=frame_loader, cache=frame_loader.data_can_fit_in_memory())
-
-
 # Initialize network
 nn = network.RNNClassifier(name='rnn-features-model-1',
                            n_steps=N_STEPS,
@@ -114,8 +52,6 @@ with tf.Session(config=config) as sess:
         train_loss = 0.
         train_batches = 0
         for input_seqs, targets in frame_loader.train:
-            print(input_seqs)
-            print(targets)
             train_loss += nn.train_op(session=sess, x=input_seqs, y=targets)
             train_batches += 1
         train_loss /= train_batches
@@ -123,8 +59,6 @@ with tf.Session(config=config) as sess:
         val_loss = 0.
         val_batches = 0
         for input_seqs, targets in frame_loader.val:
-            print(input_seqs)
-            print(targets)
             val_loss += nn.val_op(session=sess, x=input_seqs, y=targets)
             val_batches += 1
         val_loss /= val_batches
